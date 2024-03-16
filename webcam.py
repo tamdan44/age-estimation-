cf869b98693c6955fd550ff95d4b0e5fad68f9c0
tamdan44 <142738725+tamdan44@users.noreply.github.com>
2024-03-17 00:37:23 +0700

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -16,28 +16,21 @@ model_dir=r"output\\age_model.h5"
 age_model = load_model(model_dir)
 
 
-
-
-
 age_ranges = ['1-2', '3-9', '10-20', '21-27', '28-45', '46-65', '66-116']
 
-cap = cv2.VideoCapture(0)  # capture webcam
+cap = cv2.VideoCapture(0)  
 
 while (True):
     ret, img = cap.read()
     faces = face_cascade.detectMultiScale(img, 1.3, 5)
 
     for (x, y, w, h) in faces:
-        if w > 130:  # Bo qua cac mat nho
+        if w > 130: 
 
-            # Ve hinh chu nhat quanh mat
-            cv2.rectangle(img, (x, y), (x + w, y + h), (128, 128, 128), 1)  # draw rectangle to main image
+            cv2.rectangle(img, (x, y), (x + w, y + h), (128, 128, 128), 1) 
 
-            # Crop mat
-            detected_face = img[int(y):int(y + h), int(x):int(x + w)]  # crop detected face
-
+            detected_face = img[int(y):int(y + h), int(x):int(x + w)]  
             try:
-                # Them magin
                 margin = 30
                 margin_x = int((w * margin) / 100);
                 margin_y = int((h * margin) / 100)
@@ -46,13 +39,11 @@ while (True):
                 print("detected face has no margin")
 
             try:
-                # Dua mat vao mang predict
                 img_gray = cv2.cvtColor(detected_face,cv2.COLOR_BGR2GRAY)
                 age_image=cv2.resize(img_gray, (200, 200), interpolation = cv2.INTER_AREA)
                 age_input = age_image.reshape(-1, 200, 200, 1)
                 output_age = age_ranges[np.argmax(age_model.predict(age_input))]
 
-                # Ve khung thong tin
                 info_box_color = (46, 200, 255)
                 triangle_cnt = np.array(
                     [(x + int(w / 2), y), (x + int(w / 2) - 20, y - 20), (x + int(w / 2) + 20, y - 20)])
@@ -68,9 +59,8 @@ while (True):
 
     cv2.imshow('img', img)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):  # press q to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# kill open cv things
 cap.release()
 cv2.destroyAllWindows()
